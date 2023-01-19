47ea49aab00fc8e6c5e6528dddc08ce96a0e8cd9
Evyatar-dot <evyatar@dotcompliance.com>
2023-01-19 08:35:32 +0200

    Sign up started

--- src/routes/sign-in/sign-in.component.jsx
+++ src/routes/sign-in/sign-in.component.jsx
@@ -1,18 +1,41 @@
 import React from 'react'
+import { useEffect } from 'react'
+import { getRedirectResult } from 'firebase/auth'
+import SignUpForm from '../../components/sign-up-form/sign-up-form.component'
+
 import {
+    auth,
     signInWithGooglePopup,
+    signInWithGoogleRedirect,
     createUserDocumentFromAuth,
 } from '../../utils/firebase/firebase.utils'
 const SignIn = () => {
+
+    useEffect(() => {
+        let user;
+        const fetchData = async () => {
+            user = await getRedirectResult(auth);
+
+        }
+        fetchData();
+        if(user) {
+            createUserDocumentFromAuth(user)
+        }
+    }, []);
+
     const logGoogleUser = async () => {
         const { user } = await signInWithGooglePopup();
         createUserDocumentFromAuth(user)
     }
 
+
+
     return (
         <div className="sign-in">
             <h1>Sign In</h1>
-            <button onClick={logGoogleUser}>Sign In With Google</button>
+            <button onClick={signInWithGooglePopup}>Sign In With Google</button>
+            <button onClick={signInWithGoogleRedirect}>Sign In With Google Redirect</button>
+            <SignUpForm/>
         </div>
     )
 }
