4cd31695cfb1b467b59467465ed7e8ecfb0115f5
Evyatar-dot <evyatar@dotcompliance.com>
2023-07-13 10:32:36 +0300

    Syled compoenents start

--- src/components/cart-dropdown/cart-dropdown.component.jsx
+++ src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -1,12 +1,10 @@
 import { useContext } from 'react';
 import { useNavigate } from 'react-router-dom';
-import './cart-dropdown.styles.scss';
-
+import { CartDropdownContainer, CartItems, ButtonContainer} from './cart-dropdown.styles';
+import { CartContext } from '../../contexts/cart.context';
+import CartItem from '../cart-item/cart-item.component';
 import Button from '../button/button.component';
 
-import CartItem from '../cart-item/cart-item.component';
-
-import { CartContext } from '../../contexts/cart.context';
 
 const CartDropdown = () => {
     const navigate = useNavigate();
@@ -16,14 +14,14 @@ const CartDropdown = () => {
     };
     const { cartItems, setShowCart } = useContext(CartContext);
     return (
-        <div className="cart-dropdown-container">
-            <div className="cart-items">
+        <CartDropdownContainer>
+            <CartItems>
                 {cartItems.map((item) => (
                     <CartItem key={item.id} cartItem={item} />
                 ))}
-            </div>
+            </CartItems>
             <Button onClick={navigateToCheckout}> GO TO CHECKOUT </Button>
-        </div>
+        </CartDropdownContainer>
     );
 };
 
--- src/routes/navigation/navigation.component.jsx
+++ src/routes/navigation/navigation.component.jsx
@@ -9,33 +9,30 @@ import { CartContext } from '../../contexts/cart.context';
 import CartIcon from '../../components/cart-icon/cart-icon.component';
 import CartDropdown from '../../components/cart-dropdown/cart-dropdown.component';
 
-import './navigation.styles.scss';
+import { NavigationContainer, NavLink, NavLinksConatainer, LogoContainer } from './navigation.styles';
+
 const Navigation = () => {
     const { currentUser } = useContext(UserContext);
     const { showCart } = useContext(CartContext);
     return (
         <Fragment>
-            <div className="navigation">
-                <Link className="logo-container" to="/">
+            <NavigationContainer>
+                <LogoContainer>
                     <CrwnLogo className="logo" />
-                </Link>
-                <div className="nav-links-container">
-                    <Link className="nav-link" to="/shop">
-                        SHOP
-                    </Link>
+                </LogoContainer>
+                <NavLinksConatainer>
+                    <NavLink to="/shop">SHOP</NavLink>
                     {currentUser ? (
-                        <span className="nav-link" onClick={signOutUser}>
+                        <NavLink as="span" onClick={signOutUser}>
                             SIGN OUT
-                        </span>
+                        </NavLink>
                     ) : (
-                        <Link className="nav-link" to="/auth">
-                            SIGN IN
-                        </Link>
+                        <NavLink to="/auth">SIGN IN</NavLink>
                     )}
                     <CartIcon />
-                </div>
+                </NavLinksConatainer>
                 {showCart && <CartDropdown />}
-            </div>
+            </NavigationContainer>
             <Outlet />
         </Fragment>
     );
